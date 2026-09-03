dca6f61834c161ac91ab5fb6c692f445a9c507f9
Greg Drake <greg@madmallards.com>
2026-09-02 23:25:10 -0400

    Add watch-options endpoint

--- content/urls.py
+++ content/urls.py
@@ -8,4 +8,5 @@ urlpatterns = [
     path('<int:content_id>/edit/', views.edit_content, name='edit'),
     path('<int:content_id>/delete/', views.delete_content, name='delete'),
     path('import/', views.import_external_content, name='import_external'),
+    path('watch-options/', views.external_watch_options, name='watch_options'),
 ]
