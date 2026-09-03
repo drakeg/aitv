6dc892b1fcbd242d9cce771462d6ca0274717558
Greg Drake <greg@madmallards.com>
2026-09-02 22:24:55 -0400

    Add direct network content row

--- core/views.py
+++ core/views.py
@@ -4,6 +4,7 @@ from django.shortcuts import redirect, render
 
 from content.forms import QuickAddForm
 from content.models import ContentItem
+from content.providers import NETWORK_PROVIDERS
 from content.services import fetch_trending_movies, fetch_trending_tv
 from watchlist.models import Watchlist
 
@@ -45,10 +46,20 @@ def home(request):
             .values_list('content_id', flat=True)
         )
 
+    network_items = [
+        item
+        for item in db_items
+        if any(
+            availability.provider in NETWORK_PROVIDERS
+            for availability in item.availabilities.all()
+        )
+    ]
+
     context = {
         'form': form,
         'trending_movies': trending_movies[:10],
         'trending_tv': trending_tv[:10],
+        'network_items': network_items,
         'movies': [x for x in db_items if x.content_type == ContentItem.ContentType.MOVIE],
         'tv': [x for x in db_items if x.content_type == ContentItem.ContentType.TV],
         'youtube': [x for x in db_items if x.source_type == 'youtube'],
