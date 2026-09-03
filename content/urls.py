c559616a0de38c71af14622d620392fe8a40ac61
Greg Drake <greg@madmallards.com>
2026-09-03 08:59:50 -0400

    Route TMDB watch context

--- content/urls.py
+++ content/urls.py
@@ -8,4 +8,5 @@ urlpatterns = [
     path('<int:content_id>/edit/', views.edit_content, name='edit'),
     path('<int:content_id>/delete/', views.delete_content, name='delete'),
     path('import/', views.import_external_content, name='import_external'),
+    path('tmdb/<str:content_type>/<int:external_id>/watch-context/', views.tmdb_watch_context, name='tmdb_watch_context'),
 ]
