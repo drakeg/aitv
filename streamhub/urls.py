bc1f7ce8b3f2d083fa1d2748b188fba2d0cad40a
Greg Drake <greg@madmallards.com>
2026-09-03 08:59:42 -0400

    Add profile route

--- streamhub/urls.py
+++ streamhub/urls.py
@@ -2,13 +2,14 @@ from django.contrib import admin
 from django.contrib.auth import views as auth_views
 from django.urls import include, path
 
-from core.views import home, register
+from core.views import home, profile, register
 
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('accounts/login/', auth_views.LoginView.as_view(template_name='registration/login.html'), name='login'),
     path('accounts/logout/', auth_views.LogoutView.as_view(), name='logout'),
     path('accounts/register/', register, name='register'),
+    path('accounts/profile/', profile, name='profile'),
     path('content/', include('content.urls')),
     path('watchlist/', include('watchlist.urls')),
     path('', home, name='home'),
