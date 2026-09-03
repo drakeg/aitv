be093d9b2d3bc6e5fabbc59b27641250d93b51f1
Greg Drake <greg@madmallards.com>
2026-09-02 22:04:27 -0400

    Add user registration flow

--- core/views.py
+++ core/views.py
@@ -1,9 +1,27 @@
+from django.contrib.auth import login
+from django.contrib.auth.forms import UserCreationForm
 from django.shortcuts import redirect, render
 
 from content.forms import QuickAddForm
 from content.models import ContentItem
 from content.services import fetch_trending_movies, fetch_trending_tv
 from watchlist.models import Watchlist
+
+
+def register(request):
+    if request.user.is_authenticated:
+        return redirect('home')
+
+    if request.method == 'POST':
+        form = UserCreationForm(request.POST)
+        if form.is_valid():
+            user = form.save()
+            login(request, user)
+            return redirect('home')
+    else:
+        form = UserCreationForm()
+
+    return render(request, 'registration/register.html', {'form': form})
 
 
 def home(request):
